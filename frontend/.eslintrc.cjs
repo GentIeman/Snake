8d087ea5a1fb16431a4df13a068fd4d139270752
gentIeman <gentleman-2021@mail.ru>
2022-08-03 20:19:21 +0300

    init

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,8 @@
+/* eslint-env node */
+require("@rushstack/eslint-patch/modern-module-resolution")
+
+module.exports = {
+  root: true, extends: ["eslint:recommended", "plugin:vue/vue3-recommended", "prettier"], env: {
+    "vue/setup-compiler-macros": true
+  }
+}
