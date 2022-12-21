d4a658f8f85d56716856f433ea5c658dd7dea6cd
GentIeman <ilyashepelev23@gmail.com>
2022-12-21 19:16:06 +0300

    update: A global styles has been changed

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,14 +2,24 @@
   <router-view />
 </template>
 
-<script setup>
-</script>
-
 <style lang="sass">
+html,body
+  height: 100%
+
 :where(*)
   margin: 0
   padding: 0
   box-sizing: border-box
   text-decoration: none
   outline: none
+
+::-webkit-scrollbar
+  width: 8px
+
+::-webkit-scrollbar-track
+  background-color: $scrollbar-track
+
+::-webkit-scrollbar-thumb
+  background-color: $secondary
+  cursor: pointer
 </style>
